867741f46a5f89062d8e86b81a6f4eb481cb7798
Ali X <al00x@outlook.com>
2024-12-25 17:18:41 +0330

    Init

---
+++ libs/base/src/lib/base/base.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-base',
+  imports: [CommonModule],
+  templateUrl: './base.component.html',
+  styleUrl: './base.component.scss',
+})
+export class BaseComponent {}
