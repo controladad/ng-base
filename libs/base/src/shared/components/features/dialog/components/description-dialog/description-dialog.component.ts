e145483ee42520ab14a0d512128d4c4ed0e96f0d
Hamed Ebrahimi <ham.3br@gmail.com>
2025-01-30 12:23:00 +0330

    wip add translations

--- libs/base/src/shared/components/features/dialog/components/description-dialog/description-dialog.component.ts
+++ libs/base/src/shared/components/features/dialog/components/description-dialog/description-dialog.component.ts
@@ -24,7 +24,9 @@ export interface DescriptionDialogResult {
 })
 export class DescriptionDialogComponent extends BaseDialogComponent<DescriptionDialogData, DescriptionDialogResult> {
   control = formControl<string | undefined>(undefined);
-
+  TITLE_DEFAULT_TEXT = $localize`:@@base.feature.dialog.description.title:توضیحات`
+  SUBTITLE_DEFAULT_TEXT = $localize`:@@base.feature.dialog.decription.subtitle:لطفا توضیح خود را وارد کرده و دکمه ذخیره را فشار دهید.`
+  SUBMIT_DEFAULT_TEXT = $localize`:@@base.feature.dialog.description.submitButton:ذخیره`
   constructor() {
     super();
 
